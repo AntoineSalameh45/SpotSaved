4b15adac56a37ccf2fbbc1545f3d22e393ce8d21
BiG A <antoine.salameh.45@gmail.com>
2024-03-23 21:56:38 +0200

    no more nav buttons since we're using bottom tabs to navigate

--- src/screens/ScreenA/index.tsx
+++ src/screens/ScreenA/index.tsx
@@ -1,24 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import NavigationButton from '../../components/atoms/navigationButton';
 import styles from '../../GlobalStyles';
 
-const ScreenA = ({navigation}: any) => {
+const ScreenA = () => {
   return (
     <View style={styles.viewContainer}>
       <Text>Screen A</Text>
-      <View>
-        <NavigationButton
-          navigation={navigation}
-          destination="ScreenB"
-          buttonText="Go to Screen B"
-        />
-        <NavigationButton
-          navigation={navigation}
-          destination="ScreenC"
-          buttonText="Go to Screen C"
-        />
-      </View>
     </View>
   );
 };
--- src/screens/ScreenB/index.tsx
+++ src/screens/ScreenB/index.tsx
@@ -1,26 +1,13 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import styles from '../../GlobalStyles';
-import NavigationButton from '../../components/atoms/navigationButton';
 
-const ScrenB = ({navigation}: any) => {
+const ScreenB = () => {
   return (
     <View style={styles.viewContainer}>
-      <Text>ScrenB</Text>
-      <View>
-        <NavigationButton
-          navigation={navigation}
-          destination="ScreenA"
-          buttonText="Go to Screen A"
-        />
-        <NavigationButton
-          navigation={navigation}
-          destination="Camera"
-          buttonText="Go to Camera"
-        />
-      </View>
+      <Text>ScreenB</Text>
     </View>
   );
 };
 
-export default ScrenB;
+export default ScreenB;
