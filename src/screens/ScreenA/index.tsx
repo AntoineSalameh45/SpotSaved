09cecce9b4fbc6f46a599651b2da1d9b4389ce53
BiG A <antoine.salameh.45@gmail.com>
2024-03-24 18:46:41 +0200

    cleaned the page

--- src/screens/ScreenA/index.tsx
+++ src/screens/ScreenA/index.tsx
@@ -1,13 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import styles from '../../GlobalStyles';
-import CameraIcon from '../../assets/CameraSvg.svg';
 
 const ScreenA = () => {
   return (
     <View style={styles.viewContainer}>
       <Text>Screen A</Text>
-      <CameraIcon width={100} height={100} />
     </View>
   );
 };
