c87ff55e47dd0544d254589c1b538d2a562ed961
BiG A <antoine.salameh.45@gmail.com>
2024-03-25 18:59:36 +0200

    changed naming and added new screen

--- src/navigation/mainNavigator.tsx
+++ src/navigation/mainNavigator.tsx
@@ -5,6 +5,7 @@ import ScreenA from '../screens/ScreenA';
 import ScreenB from '../screens/ScreenB';
 import CameraScreen from '../screens/Camera';
 import MyTabBar from '../components/organisms/CustomTabBar';
+import AlbumGallery from '../screens/AlbumGallery';
 
 const MainStackNavigator = createBottomTabNavigator<RootStackParamList>();
 
@@ -22,8 +23,13 @@ const MainNavigator = () => {
         options={{headerShown: false}}
       />
       <MainStackNavigator.Screen
-        name="Gallery"
+        name="Api"
         component={ScreenB}
+        options={{headerShown: false}}
+      />
+      <MainStackNavigator.Screen
+        name="Gallery"
+        component={AlbumGallery}
         options={{headerShown: false}}
       />
     </MainStackNavigator.Navigator>
